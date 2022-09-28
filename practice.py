848941003f577f349eee912512c6d6d62a16f31c
hyoeun-yi <lhe323@naver.com>
2022-09-28 15:20:03 +0900

    first commit

---
+++ practice.py
@@ -0,0 +1,12 @@
+students = ["egoing","sori","maru"]
+grades = [1,2,3]
+
+print("students[1]",students[1])
+print(len(students))
+print(min(grades))
+
+import statistics
+print(statistics.mean(grades))
+
+import random
+print(random.choice(students))
